7d4be9ae70c0daa767e34b4b6f16d421039c35f4
Sven Müller <sven.mueller83@gmail.com>
2024-03-29 11:13:05 +0100

    first commit

---
+++ src/main/java/de/wicked539/unexpectedchangesetdemo/UnexpectedChangesetDemoApplication.java
@@ -0,0 +1,13 @@
+package de.wicked539.unexpectedchangesetdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnexpectedChangesetDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnexpectedChangesetDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/wicked539/unexpectedchangesetdemo/UnexpectedChangesetDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.wicked539.unexpectedchangesetdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnexpectedChangesetDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
